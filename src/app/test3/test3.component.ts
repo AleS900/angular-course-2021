d0bde7f67c517ac0f57c6df72821ce222941a198
AleS900 <santiavctf@gmail.com>
2021-08-16 23:01:15 -0400

    08/10/2021 pre-lifecycle in angular

--- src/app/test3/test3.component.ts
+++ src/app/test3/test3.component.ts
@@ -1,22 +1,36 @@
-import { Component} from '@angular/core';
+import { Component, DoCheck, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'savc-comp3',
   templateUrl: './test3.component.html',
   styleUrls: ['./test3.component.css']
 })
-export class Test3Component {
+export class Test3Component implements OnInit, OnChanges, DoCheck {
+  @Input() name;
 
-  constructor() { }
+  constructor() {
+    console.log('CONSTRUCTOR');
+  }
 
-  myName="Default Name";
-  myVarName = "Name";
-  myVarLastName = "Last Name";
+  myName = 'Default Name';
+  myVarName = 'Name';
+  myVarLastName = 'Last Name';
 
-  test3(event){
+  test3(event) {
     console.log('Event click: ', event);
     console.log('DATA: ', this.myVarName + ' ' + this.myVarLastName);
-    this.myName=this.myVarName + ' ' + this.myVarLastName;
-}
+    this.myName = this.myVarName + ' ' + this.myVarLastName;
+  }
 
+  ngOnInit() {
+    console.log('ON INIT');
+  }
+
+  ngOnChanges() {
+    console.log('ON CHANGES');
+  }
+
+  ngDoCheck() {
+    console.log('DO CHECK');
+  }
 }
