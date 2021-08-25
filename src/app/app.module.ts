dcd7ec1f50710cf423a4573faf1a6971273eee3f
AleS900 <santiavctf@gmail.com>
2021-08-24 21:37:41 -0400

    Testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { VotacionModule } from './modules/votacion/votacion.module';
-import { JuradosModule } from './modules/jurados/jurados.module';
 import { WalletsComponent } from './wallets/wallets.component';
 import { DatePipe } from './transactions/date.pipe';
 import { TransactionsComponent } from './transactions/transactions.component';
+import { JuradosModule } from './jurados/jurados.module';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, VotacionModule,JuradosModule ],
-  declarations: [ AppComponent, WalletsComponent, DatePipe, TransactionsComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    JuradosModule
+  ],
+  declarations: [ 
+    AppComponent, 
+    WalletsComponent, 
+    DatePipe, 
+    TransactionsComponent, 
+  ],
+  bootstrap:    [ 
+    AppComponent 
+  ]
 })
 export class AppModule { }
