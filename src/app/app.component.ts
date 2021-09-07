e7fd8088bc5a3eff107793c0a960363581f51c25
AleS900 <santiavctf@gmail.com>
2021-09-07 13:22:17 -0400

    modify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {BehaviorSubject, of, Subscription} from 'rxjs';
+import { filter, map,delay } from 'rxjs/operators';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  template: `
-    <router-outlet></router-outlet>
-  `
+  templateUrl: './appPart.component.html',
+  styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent {
+  id:number = 3;
+  title = 'angular2021';
+  myStatus = 'my status';
+  myNameTest = 'my name';
+  color = 'colorRandom';
+  font = '50';
+  sw=true;
+  tictock = of([1,2,3,4,5]);
+  personASub : Subscription;
+  video = 1;
+  tictock2 = new BehaviorSubject(this.video);
+  data = [1,2,3,4,5,6,7,8,9];
+  @Input() nameList = [
+    {name:'Juan', lastName:'Gutiérrez'},
+    {name:'Santiago', lastName:'Vargas'},
+    {name:'Gabriel', lastName:'Claure'},
+    {name:'Natalia', lastName:'Velarde'},
+    {name:'Daniel', lastName:'Zabalaga'},
+    {name:'Andrés', lastName:'Céspedes'}
+  ];
+  @ViewChild('myDiv1') myDiv1: ElementRef;
+  @ViewChild("myDiv2") myDiv2: ElementRef;
+  @ViewChild('myCompAleS') myCompAleS: any;
+  @ViewChild("myDiv3") myDivHW: ElementRef;
+
+  constructor(private router: Router){
+    this.pure(2,3);
+    this.pure(10,2);
+    this.pure(5,5);
 
 
+    this.impure(2,3);
+    this.impure(10,2);
+    this.impure(5,5);
+  }
+
+  onGoView2FromTS():void{
+    this.router.navigate(
+      ['view2', this.id, 'sub', 33333],
+      {queryParams: {name: 'Gabriel', lastName: 'Claure'}}
+    )
+  }
+
+  pure(a:number, b:number){
+    console.log(a + b)
+    return a + b;
+  }
+
+  impure(a:number, b:number){
+    const aux = Math.random();
+    console.log( a + b + aux)
+    return a + b + aux;
+  }
 }
---
+++ src/app/appPart2.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+
+
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', loadChildren:
+      () => import('./login/login.module').then(m => m.LoginModule)},
+  {path: 'pages', loadChildren:
+      () => import('./pages/pages.module').then(m => m.PagesModule)}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
