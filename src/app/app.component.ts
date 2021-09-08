a0deb1b2938831e54453c86725e588d8cc7f7f16
AleS900 <santiavctf@gmail.com>
2021-09-08 09:10:25 -0400

    Starting Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, VERSION } from "@angular/core";
 import { NegotiationService } from "./services/negotiation.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: "my-app",
@@ -8,59 +9,5 @@ import { NegotiationService } from "./services/negotiation.service";
 
 export class AppComponent  {
 
-  walletsArr=[];
-  transactionsArr=[];
-  btc = 0;
-  eth = 0;
-  anyLeft = true;
 
-  constructor(private negotiation:NegotiationService) { 
-    this.negotiation.getWalletsDB().subscribe(data => this.getDataW(data))
-    this.negotiation.getTransactionsDB().subscribe(data => this.getDataT(data))
-  }
-
-  getDataW(data){
-    this.walletsArr=Object.entries(data);
-    this.updateTotalMoney()
-  }
-
-  getDataT(data){
-    this.transactionsArr=Object.entries(data);
-    this.invalidTransactions()
-  }
-
-  mining(id, from, to, quantity, moneyType){
-    var toVar = this.walletsArr.filter(x => x[1]["wallet"] == to)
-    var fromVar = this.walletsArr.filter(x => x[1]["wallet"] == from)
-    var moneyTo = toVar[0][1][moneyType] + quantity
-    var moneyFrom = fromVar[0][1][moneyType] - quantity
-
-    if(moneyType === "btc"){
-      this.negotiation.mineBTC(fromVar[0][0],moneyFrom).subscribe(x => console.log(x))
-      this.negotiation.mineBTC(toVar[0][0],moneyTo).subscribe(x => console.log(x))
-    }else{
-      this.negotiation.mineETH(fromVar[0][0],moneyFrom).subscribe(x => console.log(x))
-      this.negotiation.mineETH(toVar[0][0],moneyTo).subscribe(x => console.log(x))
-    }
-    this.negotiation.delete(id).subscribe(x => console.log(x))
-    this.negotiation.getWalletsDB().subscribe(data => this.getDataW(data))
-    this.negotiation.getTransactionsDB().subscribe(data => this.getDataT(data))
-  
-    this.updateTotalMoney();
-    window.location.reload();
-  }
-
-  updateTotalMoney(){
-    this.eth = 0
-    this.btc = 0
-    for(var i in this.walletsArr){
-      this.eth = this.walletsArr[i][1]["eth"] + this.eth
-      this.btc = this.walletsArr[i][1]["btc"] + this.btc
-    }
-  }
-
-  invalidTransactions():boolean{
-    return this.transactionsArr.find(item => item[1]['mineType'] !== 'PoS' ||
-    item[1]['miner'] > 20) === undefined
-  }
 }
