f29d4ef5362ca3075ad1955aaa3332607424486a
AleS900 <santiavctf@gmail.com>
2021-09-16 02:17:27 -0400

    09/15/2021 logout, autologin, autologout

--- src/app/pages/shared/components/header/header.component.ts
+++ src/app/pages/shared/components/header/header.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../../../core/services/auth.service";
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  onLogout():void{
+    this.authService.logout();
   }
 
 }
